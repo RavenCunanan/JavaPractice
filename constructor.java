cf9998230d982e15816ad0d7ced5ed10f03e318a
Raven Cunanan <63638637+RavenCunanan@users.noreply.github.com>
2025-04-07 21:28:35 -0700

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,33 @@
+// The Neopet class has been created for you! 💖
+
+class Neopet {
+	String name;
+  String species;
+	String color;
+	char gender; 
+	Boolean hasScarf;	
+
+    // Add additional attributes here 💖
+    double strength;
+    double defense;
+    double movement;
+
+    // Add a constructor here 💖
+    Neopet(){
+        strength = 0.0;
+        defense = 0.0;
+        movement = 0.0;
+    }
+    
+}
+
+public class Main {
+    public static void main(String[] args) {
+       Neopet gravatia = new Neopet();
+    
+       // Add print statement here 💖
+       System.out.println("S: " + gravatia.strength);
+       System.out.println("D: " + gravatia.defense);
+       System.out.println("M: " + gravatia.movement);
+    }
+}
